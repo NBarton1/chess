7380582735dd9661d438ba7b94f3cbe1ab919271
nmbar <nmbarton1@loyola.edu>
2022-10-25 22:45:08 -0400

    made it worse

--- king.py
+++ king.py
@@ -7,29 +7,29 @@ def king(square, owned_pieces=owned_pieces):
     rank = (square // 8) + 1
     file = (square % 8) + 1
 
-    move_left = file >= 2
-    move_right = file <= 7
-    move_down = rank >= 2
-    move_up = rank <= 7
+    left = file >= 2
+    right = file <= 7
+    down = rank >= 2
+    up = rank <= 7
 
     possible_moves = []
 
     # Making list of possible moves
-    if move_left and (square-1 not in owned_pieces):
+    if left and (square-1 not in owned_pieces):
         possible_moves.append(square-1)
-    if move_right and (square+1 not in owned_pieces):
+    if right and (square+1 not in owned_pieces):
         possible_moves.append(square+1)
-    if move_up and (square+8 not in owned_pieces):
+    if up and (square+8 not in owned_pieces):
         possible_moves.append(square+8)
-    if move_down and (square-8 not in owned_pieces):
+    if down and (square-8 not in owned_pieces):
         possible_moves.append(square-8)
-    if move_up and move_left and (square+7 not in owned_pieces):
+    if up and left and (square+7 not in owned_pieces):
         possible_moves.append(square+7)
-    if move_up and move_right and (square+9 not in owned_pieces):
+    if up and right and (square+9 not in owned_pieces):
         possible_moves.append(square+9)
-    if move_left and move_down and (square-9 not in owned_pieces):
+    if left and down and (square-9 not in owned_pieces):
         possible_moves.append(square-9)
-    if move_down and move_right and (square-7 not in owned_pieces):
+    if down and right and (square-7 not in owned_pieces):
         possible_moves.append(square-7)
 
     return possible_moves
